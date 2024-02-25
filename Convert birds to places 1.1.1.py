b110e8de38ab8b5ec7ec6ce1e4de15990b7dacad
Anyll Markevich <anyll@Anylls-MacBook-Pro-1477.local>
2024-02-24 21:16:57 -0700

    Made a bunch of major changes! Notably, I fixxed an issue where data in the first 10 rows were being ignored and created a system for inserting blank data rows for places with no bird observations as QGIS is having trouble displaying NULL values correctly.

--- Convert birds to places 1.1.1.py
+++ Convert birds to places 1.1.1.py
@@ -7,6 +7,50 @@ columns = file.columns
 file = file.values.tolist()
 #print(file)
 
+staticFile = file
+for it, entry in enumerate(file):
+    if len(str(entry[3])) == 3:
+        file[it][3] = "0" + str(entry[3])
+    elif len(str(entry[3])) == 2:
+        file[it][3] = "00" + str(entry[3])
+    elif len(str(entry[3])) == 1:
+        file[it][3] = "000" + str(entry[3])
+    elif str(entry[3]) == "":
+        file[it][3] = "0000" + str(entry[3])
+    else:
+        file[it][3] = str(entry[3])
+
+#print(file)
+staticFile = file
+insertBlankValues = input("Would you like to insert blank values (\"0\") for plots without bird observations? (y/n)")
+
+maxRow = 0
+MaxColumn = 0
+
+if insertBlankValues == "y":
+    insertBlankValues = True
+    maxRow = input("How many rows does you data have?")
+    maxColumns = input("How many columns does you data have?")
+else:
+    insertBlankValues = False
+
+
+if insertBlankValues:
+    rowValue = ""
+    columnValue = ""
+    theoreticalLocationStringList = []
+    for i in range(0, int(maxRow)):
+        for j in range(0, int(maxColumns)):
+            rowValue = str(i)
+            while len(rowValue) < len(str(maxRow)):
+                rowValue = "0" + rowValue
+            columnValue = str(j)
+            while len(columnValue) < len(str(maxColumns)):
+                columnValue = "0" + columnValue
+            theoreticalLocationStringList.append(rowValue + columnValue)
+
+print(theoreticalLocationStringList)
+
 birdSpeciesList = []
 locationList = []
 for i in file:
@@ -25,7 +69,7 @@ for i in file:
         if j == i[3]:
             itemFound = True
     if itemFound == False:
-        locationList.append(i[3])
+        locationList.append(str(i[3]))
 
 print(locationList)
 
@@ -49,7 +93,7 @@ for it, birdListing in enumerate(file):
             for it3, birdRow in enumerate(staticReformattedList):
                 if birdListing[3] == birdRow[0]:
                     if it3 > 0 and it2 > 0:
-                        print(birdListing[1])
+                        # print(birdListing[1])
                         reformattedList[it3][it2] = reformattedList[it3][it2] + birdListing[1]
                         for it4, birdAdder in enumerate(reformattedList[it3]):
                             if it4> 0 and it4 < len(reformattedList[it3])-1:
@@ -57,9 +101,31 @@ for it, birdListing in enumerate(file):
                         reformattedList[it3][len(reformattedList[it3])-1] = birdTotal
                         birdTotal = 0
 
+staticReformattedList = reformattedList
+for row, i in enumerate(staticReformattedList):
+    for column, j in enumerate(i):
+        if len(str(j)) < 1:
+            reformattedList[row][column] = 0
+
+staticReformattedList = reformattedList
+if insertBlankValues:
+    valueFound = False
+    tempList = []
+    for i in theoreticalLocationStringList:
+        tempList = []
+        valueFound = False
+        for j in reformattedList:
+            if i == j[0]:
+                valueFound = True
+        if not valueFound:
+            tempList.append(i)
+            for j in birdSpeciesList:
+                tempList.append(0)
+            tempList.append(0)
+            reformattedList.append(tempList)
+
 outPath = inPath.removesuffix(".csv")
 outPath += "_Reformatted_For_QGIS.csv"
-
 columns = reformattedList[0]
 reformattedList.pop(0)
 saveFile = pandas.DataFrame(reformattedList, columns=columns)
